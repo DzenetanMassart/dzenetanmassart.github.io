58d496fb23dd718edd9cd0a4aeb2675ed63c91f8
DzenetanMassart <dzenetan.massart@gmail.com>
2019-05-17 09:33:07 +0200

    Les applications sont à génération automatisée par le Javascript !

---
+++ assets/js/apps.js
@@ -0,0 +1,44 @@
+let app = [{
+        "title": "Parlote",
+        "badgestyle": "badge-warning",
+        "badge": "Utilise le microphone",
+        "desc": "C'est une application pour faire parler un personnage avec notre microphone en live !",
+        "link": "https://dzenetanmassart.github.io/Parlote/"
+    },
+    {
+        "title": "Tricks and Tips",
+        "badgestyle": "badge-secondary",
+        "badge": "Compile des apps et de la doc'",
+        "desc": "C'est une application pour chercher des applications et de la documentation autour des sites parlant de CSS,Boostrap,HTML, Javascript,...",
+        "link": "https://dzenetanmassart.github.io/Tricks-and-Tips/"
+    },
+    {
+        "title": "Navigateurs",
+        "badgestyle": "badge-danger",
+        "badge": "Choix de navigateur",
+        "desc": "C'est une application pour effectuer des recherches avec choix du navigateur avec pour base un code trouvé sur un vieux site ! ",
+        "link": "https://dzenetanmassart.github.io/Navigateurs/"
+    },
+    {
+        "title": "Cookie Clicker JS",
+        "badgestyle": "badge-dark",
+        "badge": "Collectez des cookies en JS !",
+        "desc": "C'est une application qui a été créé en guise d'épreuve sur nos capacités à créer des jeux en JS+CANVAS/HTML/CSS , la particularité de ma version est son rendu visuel !",
+        "link": "https://dzenetanmassart.github.io/JS-Cookie-Clicker/"
+    },
+    {
+        "title": "JS Cookie Clicker REVENGE",
+        "badgestyle": "badge-dark",
+        "badge": "On revient pour la nouvelle version du Cookie Clicker en JS ! ",
+        "desc": "Une version bien plus propre (par la voie de la sobriété) et facile à éditer du premier Cookie Clicker  en javascript où le design demeure adaptable sur toutes les types d'écrans !",
+        "link": "https://dzenetanmassart.github.io/JS-Cookie-Clicker-REVENGE/"
+    },
+    {
+        "title": "Le jeu du pendu",
+        "badgestyle": "badge-info",
+        "badge": "Trouvez le mot !",
+        "desc": "C'est une application qui a été créé dans la même veine que Cookie Clicker JS ! Le but est de trouver le mot mystère... sauf que l'application n'a pas de fin car,même si le mot est trouvé, l'application ne désactive pas le bouton et n'offre pas un bon feedback pour féliciter le joueur!",
+        "link": "https://dzenetanmassart.github.io/le_jeu_du_pendu/"
+    }
+
+]
---
+++ assets/js/generate_apps.js
@@ -0,0 +1,21 @@
+let generate = " ";
+
+
+
+for (let ii = 0; ii < app.length; ii++) {
+    let apps = (app[ii]);
+
+    generate += '<article class="card"><h4 class="card-title">' +
+        apps.title +
+        '</h4><span class="badge ' +
+        apps.badgestyle +
+        '">' +
+        apps.badge +
+        '</span><div class="card-body"><p class="card-text">' +
+        apps.desc +
+        '</p><a href="' +
+        apps.link +
+        '" class="btn btn-primary">Lien</a></div></article>';
+}
+
+document.getElementById("generate").innerHTML += generate;
