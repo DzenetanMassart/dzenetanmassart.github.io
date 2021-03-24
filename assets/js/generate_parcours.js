24d7625b19ce6bcace9a528b499f8de5d7d1088e
Dzénetan Massart <dzenetan.massart@gmail.com>
2021-03-24 20:01:51 +0100

    Amélioration de la génération du parcours
    Maintenant, les panels s'enchaînent l'un au dessus de l'autre en "mobile" !

---
+++ assets/js/generate_parcours.js
@@ -0,0 +1,22 @@
+let generate_parc = " ";
+
+
+
+for (let iii = 0; iii < parcours.length; iii++) {
+    let parci = (parcours[iii]);
+
+    generate_parc += ' <article style=" background: radial-gradient(circle, rgba(255,255,255,1) 25%, rgba(255,255,255,0) 100%), url(' + img_link[iii] + ' )" > <img src=' + img_link[iii] + ' alt="' + parci.alt + '"></article><article><h3>' + parci.title + '</h3>';
+    generate_parc += '<p>' + parci.para1 + '<p>';
+
+    if (parci.para2 != undefined) {
+        generate_parc += '<p>' + parci.para2 + '<p>';
+    }
+    if (parci.para3 != undefined) {
+        generate_parc += '<p>' + parci.para3 + '<p>';
+    }
+
+
+    generate_parc += '</article>';
+}
+
+document.getElementById("parc").innerHTML += generate_parc;
---
+++ assets/js/parcours.js
@@ -0,0 +1,54 @@
+/* Le parcours en lien d'image */
+const img_link = [
+    "assets/image/parcours/2007.png",
+    "assets/image/parcours/2008.png",
+    "assets/image/parcours/2010.png",
+    "assets/image/parcours/2011.png",
+    "assets/image/parcours/2014.png",
+    "assets/image/parcours/2016.png",
+    "assets/image/parcours/2019.png",
+    "assets/image/parcours/2020.png"
+];
+
+/* Le parcours en texte */
+const parcours = [{
+    "alt": "PaintXP",
+    "title": "2007: L'image",
+    "para1": "C'est en 2007 que je commence à m'amuser avec Paint de WindowsXP et où j'ai commencé à faire des montages-photos et du dessin.",
+    "para2": "Picasa sera également employé pour palier le manque d'effet de traitement visuel."
+}, {
+    "alt": "Windows Movie Maker",
+    "title": "2008: La vidéo",
+    "para1": "C'est en Avril 2008 que je commence à m'amuser avec Windows Movie Maker et où j'ai commencé à faire des YouTube Poop et des podcasts.",
+    "para2": "Audacity sera également employé pour palier le manque d'effet de traitement audio."
+}, {
+    "alt": "Google SketchUp",
+    "title": "2010: La 3D",
+    "para1": "C'est en Novembre 2010 que je commence à m'amuser avec Google SketchUp et où j'ai commencé à faire des mises en scène en 3D.",
+}, {
+    "alt": "Magix Video Deluxe et PaintDotNet",
+    "title": "2011: Magix Video Deluxe et PaintDotNet",
+    "para1": "C'est en Juillet 2011 que je commence à me séparer de Windows Movie Maker et de Paint pour bien plus puissant et donc moins contraignant.",
+    "para2": "TheGimp fera également partie des logiciels que j'emplois mais seulement pour la création et la modification de gif animé."
+}, {
+    "alt": "Photoshop",
+    "title": "2014: Photoshop CS6",
+    "para1": "C'est en 2014 que je me tourne vers la suite Adobe pour Photoshop afin de pouvoir faire certains effets non-disponible sur PaintDotNet.",
+    "para2": "Je me sépare de TheGimp pour des raisons de lenteur et de doublon avec PaintDotNet et Photoshop."
+}, {
+    "alt": "Adobe Illustraor, After Effect & InDesign. Autodesk 3DSmax, Maya. ",
+    "title": "2016: Illustrator, After Effect , InDesign , 3DSmax et Maya",
+    "para1": "C'est en 2016, à la Haute École Albert Jacquard en section \"infographie\" que j'apprend à me servir du reste de la suite Adobe et que je découvre la suite d'Autodesk.",
+    "para2": "Je me sépare de TheGimp pour des raisons de lenteur et de doublon avec PaintDotNet et Photoshop.",
+    "para3": "Je fais la rencontre du logiciel Notepad++ en cours de Javascript et je m'en sers pour commencer à créer des petites pages en HTML de temps en temps."
+}, {
+    "alt": "Adobe Premiere Pro CS6 & Visual Studio Code",
+    "title": "2019: Adobe Premiere Pro et Visual Studio Code",
+    "para1": "C'est en 2019 , suite à la perte de Magix Video Deluxe suite à un problème de ré-installation, que je passe enfin à Adobe Premiere Pro qui me permet de faire des vidéos avec encore plus d'ambition de part son système d'imbriquation et ses effets multiples et polyvalents. C'est aussi cette année-là où je fais ma formation à BeCode où j'apprend à me servir de Visual Studio Code pour coder mon site web, des applications et même rédiger mon C.V. .",
+    "para2": "Je me sépare de Notepad++ devenu inutile face à la machine à gaz qu'est mon Visual Studio Code."
+}, {
+    "alt": "OBS Studio",
+    "title": "2020: OBS Studio",
+    "para1": "C'est en 2020 , afin d'optimiser mes captures d'écrans et ainsi faire des tutoriels beaucoup plus fluides, que je me suis procuré le logiciel \"OBS studio \" .",
+    "para2": "Je fais la rencontre du logiciel FireAlpaca qui est compatible avec ma tablette graphique contrairement à mon Adobe Photoshop CS6."
+}];
