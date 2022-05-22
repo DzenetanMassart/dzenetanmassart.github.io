bf8fc12ce4b78f76c2b2aaaafa4915705355477b
DzenetanMassart <dzenetan.massart@gmail.com>
2022-05-22 05:01:32 +0200

    Mise à jour de fin Mai 2022
    Suppression du Porte-Folio, amélioration du CV et ajout de logiciels et capacités.

--- assets/cv/cv.js
+++ assets/cv/cv.js
@@ -1,14 +1,96 @@
-const url_o = window.location.href;
-let url_result = url_o.substring(url_o.lastIndexOf("#") + 1);
+const colorisation = function() {
+    const tD = document.getElementById("tD");
+    const video = document.getElementById("video");
+    const photo = document.getElementById("photo");
+    const Gi = document.getElementById("Gi");
+    const mep = document.getElementById("mep");
+    const optiweb = document.getElementById("optiweb");
+    const tdd = document.getElementById("tdd");
+    const coding = document.getElementById("coding");
+    tD.style.backgroundColor = "red";
+    video.style.backgroundColor = "red";
+    photo.style.backgroundColor = "black";
+    Gi.style.backgroundColor = "black";
+    mep.style.backgroundColor = "#4c85bd";
+    optiweb.style.backgroundColor = "#4c85bd";
+    tdd.style.backgroundColor = "#4c85bd";
+    coding.style.backgroundColor = "yellow";
+    coding.style.color = "black";
+}
+colorisation()
 
-document.addEventListener("DOMContentLoaded", function() {
-    if (url_result === "") {};
-    if (url_result === "Secrétaire") { generator(com) };
-    if (url_result === "Infographiste") { generator(video + photo + code) };
-    if (url_result === "Codeur") { generator(code) };
-    if (url_result === "Monteur") { generator(video) };
-    if (url_result === "Photographe") { generator(photo) };
-    if (url_result === "CommunityManager") { generator(com) };
-    if (url_result === "EmployéDeService") { generator(com) };
-    if (url_result === "Vidéaste") { generator(video + photo) };
+document.getElementById('metier').addEventListener('change', function() {
+
+
+
+
+    switch (this.value) {
+        case "Video Maker":
+            document.getElementById("met").innerText = "Pour " + this.value;
+            tD.style.display = "inline-block";
+            video.style.display = "inline-block";
+            photo.style.display = "inline-block";
+            Gi.style.display = "inline-block";
+            mep.style.display = "inline-block";
+            optiweb.style.display = "none";
+            tdd.style.display = "none";
+            coding.style.display = "none";
+            break;
+        case "Photographe":
+            document.getElementById("met").innerText = "Pour " + this.value;
+            tD.style.display = "none";
+            video.style.display = "none";
+            photo.style.display = "inline-block";
+            Gi.style.display = "inline-block";
+            mep.style.display = "none";
+            optiweb.style.display = "none";
+            tdd.style.display = "none";
+            coding.style.display = "none";
+            break;
+        case "Community Manager":
+            document.getElementById("met").innerText = "Pour " + this.value;
+            tD.style.display = "inline-block";
+            video.style.display = "inline-block";
+            photo.style.display = "inline-block";
+            Gi.style.display = "inline-block";
+            mep.style.display = "inline-block";
+            optiweb.style.display = "inline-block";
+            tdd.style.display = "inline-block";
+            coding.style.display = "inline-block";
+            break;
+        case "Employé de Service":
+            document.getElementById("met").innerText = "Pour " + this.value;
+            tD.style.display = "none";
+            video.style.display = "none";
+            photo.style.display = "none";
+            Gi.style.display = "none";
+            mep.style.display = "inline-block";
+            optiweb.style.display = "none";
+            tdd.style.display = "inline-block";
+            coding.style.display = "none";
+            break;
+        case "Codeur Javascript":
+            document.getElementById("met").innerText = "Pour " + this.value;
+            tD.style.display = "none";
+            video.style.display = "none";
+            photo.style.display = "none";
+            Gi.style.display = "none";
+            mep.style.display = "none";
+            optiweb.style.display = "none";
+            tdd.style.display = "none";
+            coding.style.display = "inline-block";
+            break;
+        default:
+
+            document.getElementById("met").innerText = "";
+            tD.style.display = "inline-block";
+            video.style.display = "inline-block";
+            photo.style.display = "inline-block";
+            Gi.style.display = "inline-block";
+            mep.style.display = "inline-block";
+            optiweb.style.display = "inline-block";
+            tdd.style.display = "inline-block";
+            coding.style.display = "inline-block";
+
+    }
 });
